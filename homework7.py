ccc46ba2466dc7e879889b493a41b0c38d2315ff
xdvt <xdvt1337@gmail.com>
2024-07-03 20:53:47 +0700

    Add files via upload

---
+++ homework7.py
@@ -0,0 +1,9 @@
+first = int(input("Введите первое число: "))
+second = int(input("Введите второе число: "))
+third = int(input("Введите третье число: "))
+if first == second == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+elif first != second != third:
+    print(0)
